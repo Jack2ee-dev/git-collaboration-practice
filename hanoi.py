3d35c84d20bdcbe1f6969a7810f67c694a31361c
kimjeongwon <kimjeongwonn@gmail.com>
2021-04-01 16:35:48 +0900

    feat: Implimented Hanoi Function with static input
    1. Hanoi Funcion Impliment
    2. execution Hanoi Function 3rd floor

--- hanoi.py
+++ hanoi.py
@@ -1,2 +1,8 @@
 def hanoi(n, a, b, c):
-    pass
+    if n == 1:
+        return [[a, c]]
+    return hanoi(n-1, a, c, b) + [[a, c]] + hanoi(n-1, b, a, c)
+
+
+if __name__ == "__main__":
+    print(hanoi(3, 1, 2, 3))
